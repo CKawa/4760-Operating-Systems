2e0667d663fbd6d1225014ce59427beaca12ddc7
Charlotte Kawa <o2-kawa@hoare7.cs.umsl.edu>
2018-02-01 19:02:33 -0600

    Project1 - fan files

---
+++ simplefan.c
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////
+//Charlotte Kawa
+//cekxb3
+//Project1
+//Process Fans
+////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main (int argc, char *argv[]) {
+	pid_t childpid = 0;
+	int i, n;
+
+	if (argc != 2){
+		
+	}
+
+
+
+
+}
